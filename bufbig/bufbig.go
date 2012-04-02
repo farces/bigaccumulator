c2baa928208cdff1c4fb6d3005fd7d331a4722bc
Mark Johnson <amckhome@tpg.com.au>
2012-04-02 23:29:55 +1000

    "go fmt"

--- bufbig/bufbig.go
+++ bufbig/bufbig.go
@@ -6,31 +6,37 @@ import (
 
 /* basic big.Int wrapper with an intermediary int64 accumulator */
 type BigAccumulator struct {
-    t int64
-    dirty bool
-	Val *big.Int
+	t     int64
+	dirty bool
+	Val   *big.Int
 }
 
 //add an int to a bigint, buffers additions and flushes when overflow detected
 func (z *BigAccumulator) AddInt(y int) *BigAccumulator {
-    if (z.t >= (9223372036854775807 - int64(y)))  { z.flush() }
-    z.t = z.t+int64(y)
-    z.dirty = true
-    return z
+	if z.t >= (9223372036854775807 - int64(y)) {
+		z.flush()
+	}
+	z.t = z.t + int64(y)
+	z.dirty = true
+	return z
 }
 
 //flush value from z.t to val (big.Int)
 func (z *BigAccumulator) flush() {
-    if !z.dirty { return }
-    z.Val.Add(z.Val, big.NewInt(z.t))
-    z.t = 0
-    z.dirty = false
+	if !z.dirty {
+		return
+	}
+	z.Val.Add(z.Val, big.NewInt(z.t))
+	z.t = 0
+	z.dirty = false
 }
 
 //returns underlying big.Int value, after flushing any buffered value
 func (z *BigAccumulator) Value() *big.Int {
-    if z.dirty { z.flush() }
-    return z.Val
+	if z.dirty {
+		z.flush()
+	}
+	return z.Val
 }
-//
 
+//
--- bufbig_example.go
+++ bufbig_example.go
@@ -2,15 +2,14 @@ package main
 
 import "github.com/farces/dumb/bufbig"
 import (
-    "fmt"
-    "math/big"
+	"fmt"
+	"math/big"
 )
 
-
 func main() {
-    x := bufbig.BigAccumulator{Val:new(big.Int)}
-    for y := 0; y<250000; y++ {
-        x.AddInt(1)
-    }
-    fmt.Println(x.Value())
+	x := bufbig.BigAccumulator{Val: new(big.Int)}
+	for y := 0; y < 250000; y++ {
+		x.AddInt(1)
+	}
+	fmt.Println(x.Value())
 }
