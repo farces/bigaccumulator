3f17aae5f2236c42ba399f4090db70eaf42d3264
Mark Johnson <amckhome@tpg.com.au>
2012-04-03 02:27:18 +1000

    fix example to use Init()

--- bufbig_example.go
+++ bufbig_example.go
@@ -3,11 +3,10 @@ package main
 import "github.com/farces/dumb/bufbig"
 import (
 	"fmt"
-	"math/big"
 )
 
 func main() {
-	x := bufbig.BigAccumulator.Init() //init BigAccumulator (Val init required)
+	x := new(bufbig.BigAccumulator).Init() //init BigAccumulator (Val init required)
 	for y := 0; y < 250000; y++ {
 		x.AddInt(1)
 	}
