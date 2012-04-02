b14acffb485f208c3837983fe94564d3594f9708
Mark Johnson <amckhome@tpg.com.au>
2012-04-03 01:37:35 +1000

    variable name changes to be more descriptive

--- bufbig/bufbig.go
+++ bufbig/bufbig.go
@@ -7,39 +7,37 @@ import (
 
 /* basic big.Int wrapper with an intermediary int64 accumulator */
 type BigAccumulator struct {
-	t     int64 //intermediate acc
-	dirty bool  //is flush required?
-	Val   *big.Int
+	t_acc     int64 //intermediate acc
+	req_flush bool  //is flush required?
+	Val       *big.Int
 }
 
 //add an int to a bigint, buffers additions and flushes when overflow detected
-func (z *BigAccumulator) AddInt(y int) *BigAccumulator {
+func (x *BigAccumulator) AddInt(y int) *BigAccumulator {
 	n := int64(y)
-	if y > 0 && (z.t > (math.MaxInt64 - n)) {
-		z.flush()
-	} else if y < 0 && (z.t < (math.MinInt64 - n)) {
-		z.flush()
+	if y > 0 && (x.t_acc > (math.MaxInt64 - n)) {
+		x.flush()
+	} else if y < 0 && (x.t_acc < (math.MinInt64 - n)) {
+		x.flush()
 	}
-	z.t = z.t + n
-	z.dirty = true
-	return z
+	x.t_acc = x.t_acc + n
+	x.req_flush = true
+	return x
 }
 
-func (z *BigAccumulator) flush() {
-	if !z.dirty {
+func (x *BigAccumulator) flush() {
+	if !x.req_flush {
 		return
 	}
-	z.Val.Add(z.Val, big.NewInt(z.t))
-	z.t = 0
-	z.dirty = false
+	x.Val.Add(x.Val, big.NewInt(x.t_acc))
+	x.t_acc = 0
+	x.req_flush = false
 }
 
 //returns underlying big.Int value, after flushing any buffered value
-func (z *BigAccumulator) Value() *big.Int {
-	if z.dirty {
-		z.flush()
+func (x *BigAccumulator) Value() *big.Int {
+	if x.req_flush {
+		x.flush()
 	}
-	return z.Val
+	return x.Val
 }
-
-//
